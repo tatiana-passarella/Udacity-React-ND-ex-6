8a82b82bf649bc7b0ec503c5572cda7c6d894a32
tatiana-passarella <tatiana.passarella@gmail.com>
2020-12-11 09:34:32 +0000

    Typo correction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import logo from './logo.svg';
+import './App.css';
 import CreateNewItem from './CreateNewItem';
 import ItemList from './ItemList';
 import DeleteLastItem from './DeleteLastItem';
